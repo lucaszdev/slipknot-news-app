a5155677ab932ade3d169a5024270cbe58799382
Lucas Eduardo <53530532+lucasZduality@users.noreply.github.com>
2019-08-08 06:24:30 -0300

    Update pages/About/index.js

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -20,7 +20,7 @@ export default function About() {
         <VersionText>Version 0.0.1</VersionText>
         <Image source={Logo} />
       </ContentContainer>
-      <ButtonGit onPress={() => Linking.openURL('https://github.com/lucasz-si/slipknot-news-app')}>
+      <ButtonGit onPress={() => Linking.openURL('https://github.com/lucasZduality/slipknot-news-app')}>
         <ButtonText>Repositório do Projeto</ButtonText>
       </ButtonGit>
     </Container>
